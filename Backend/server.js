69729942c7424327a516cae72d98693b3d36c19a
Jahongir_R <153822463+Jahongir-Rustamov@users.noreply.github.com>
2025-04-11 16:19:22 +0500

    Updated for Deployment

--- Backend/server.js
+++ Backend/server.js
@@ -9,12 +9,13 @@ import StudentsSections from "./routes/StudentsSections.route.js";
 import ProfileSection from "./routes/ProfileSection.route.js";
 import GetStatistics from "./routes/GetStatistics.js";
 import { connectDB } from "./Databases/db.js";
+import path from "path";
 const app = express();
 config();
 
 app.use(express.json());
 app.use(cookieParser());
-
+const __dirname = path.resolve();
 app.use("/api/auth", AuthSections);
 app.use("/api/admin", AdminSections);
 app.use("/api/teacher", TeacherSections);
@@ -22,7 +23,12 @@ app.use("/api/get_tests", GetAllOfTests);
 app.use("/api/student", StudentsSections);
 app.use("/api/get/Statistics", GetStatistics);
 app.use("/api/profile", ProfileSection);
-
+if (process.env.Node_Env === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../Frontend", "dist", "index.html"));
+  });
+}
 const port = process.env.port || 3000;
 app.listen(port, () => {
   connectDB();
